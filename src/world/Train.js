01da32e3a38e1e185093ee09cccb83adefe270ec
Linzee <d.gmiterko@gmail.com>
2017-03-20 22:07:59 +0100

    LeaningTrain

--- src/world/Train.js
+++ src/world/Train.js
@@ -47,12 +47,32 @@ export default class Train extends PIXI.Container {
 			this.children[i].x = this.children[i-1].x + dx;
 			this.children[i].y = this.children[i-1].y + dy;
 			this.children[i].rotation = getAngle(this.children[i].x);
+
+			var skew = this.children[i].y - getY(this.children[i].x);
+
+			if(Math.abs(skew) > Train.CART_MAX_SKEW) {
+				this.children[i].y = getY(this.children[i].x) + Math.sign(skew) * Train.CART_MAX_SKEW;
+				skew = Math.sign(skew) * Train.CART_MAX_SKEW;
+			}
+
+			this.children[i].children[0].y = -Train.CART_HEIGHT2 - skew;
 		}
 	}
 
 	addCart() {
 		var cartContainer = new PIXI.Sprite();
 		
+		var cartBottom = new PIXI.Graphics();
+		cartBottom.width = Train.CART_WIDTH;
+		cartBottom.width = Train.CART_HEIGHT;
+		cartBottom.beginFill(0x999900);
+		cartBottom.drawRect(0, 0, Train.CART_WIDTH, Train.CART_HEIGHT);
+
+		cartBottom.x = -Train.CART_WIDTH2;
+		cartBottom.y = -Train.CART_HEIGHT2;
+
+		cartContainer.addChild(cartBottom);
+
 		var cartTop = new PIXI.Graphics();
 		cartTop.width = Train.CART_WIDTH;
 		cartTop.width = Train.CART_HEIGHT;
@@ -64,16 +84,8 @@ export default class Train extends PIXI.Container {
 
 		cartContainer.addChild(cartTop);
 
-		var cartBottom = new PIXI.Graphics();
-		cartBottom.width = Train.CART_WIDTH;
-		cartBottom.width = Train.CART_HEIGHT;
-		cartBottom.beginFill(0xFFFF00);
-		cartBottom.drawRect(0, 0, Train.CART_WIDTH, Train.CART_HEIGHT);
-
-		cartBottom.x = -Train.CART_WIDTH2;
-		cartBottom.y = -Train.CART_HEIGHT2;
-
-		cartContainer.addChild(cartBottom);
+		cartContainer.x = -9999999;
+		cartContainer.y = 0;
 
 		this.addChild(cartContainer);
 	}
@@ -100,3 +112,4 @@ Train.CART_HEIGHT = 10;
 Train.CART_WIDTH2 = Train.CART_WIDTH / 2;
 Train.CART_HEIGHT2 = Train.CART_HEIGHT / 2;
 Train.CART_DELAY = 32;
+Train.CART_MAX_SKEW = Train.CART_HEIGHT / 3;
