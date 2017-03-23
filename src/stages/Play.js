9ee7f9ccdc2232690a90703b5074f02cd46ba031
Linzee <d.gmiterko@gmail.com>
2017-03-23 11:04:48 +0100

    CrashingTrain

--- src/stages/Play.js
+++ src/stages/Play.js
@@ -28,15 +28,24 @@ export default class StagePlay extends PIXI.Container {
 		this.cartsText.x = 16;
 		this.cartsText.y = 16;
 
+		this.crashedText = new PIXI.Text("Crashed", new PIXI.TextStyle({fontSize: 40, fill: '#FFFFFF'}));
+		this.crashedText.x = this.settings.width / 2;
+		this.crashedText.y = this.settings.height / 2;
+		this.crashedText.anchor.x = 0.5;
+		this.crashedText.anchor.y = 0.5;
+		this.crashedText.visible = false;
+
 		this.addChild(this.world);
 		this.addChild(this.train);
 		this.addChild(this.switchCursor);
 		this.addChild(this.cartsText);
+		this.addChild(this.crashedText);
 
 		this.keyUp = new KeyListener(38);
 		this.keyDown = new KeyListener(40);
 		this.keyLeft = new KeyListener(37);
 		this.keyRight = new KeyListener(39);
+		this.keySpace = new KeyListener(32);
 	}
 
 	tick() {
@@ -44,46 +53,68 @@ export default class StagePlay extends PIXI.Container {
 		this.switchCursor.recalculatePosition();
 
 		//move camera
-		if(this.train.children[0].x > this.cameraPosPercentage * this.settings.width) {
-			var camPos = this.cameraPosPercentage * this.settings.width - this.train.children[0].x;
-			this.world.x = camPos;
-			this.train.x = camPos + World.PIECE_WIDTH2;
-			this.switchCursor.x = camPos;
+		var camPos = this.cameraPosPercentage * this.settings.width - this.train.children[0].x;
+		if(camPos > 0) {
+			camPos = 0;
 		}
-
-		if(this.keyUp.isDown) {
-			this.switchCursor.switchCursor(0);
-		}
-		if(this.keyDown.isDown) {
-			this.switchCursor.switchCursor(1);
-		}
-		if(this.keyLeft.isDown) {
-			this.train.speed -= Train.SPEED_CHANGE_STEP;
-		}
-		if(this.keyRight.isDown) {
-			this.train.speed += Train.SPEED_CHANGE_STEP;
+		camPos -= World.PIECE_WIDTH;
+		this.world.x = camPos;
+		this.train.x = camPos + World.PIECE_WIDTH2;
+		this.switchCursor.x = camPos;
+		
+		if(!this.train.isCrashed) {
+			if(this.keyUp.isDown) {
+				this.switchCursor.switchCursor(0);
+			}
+			if(this.keyDown.isDown) {
+				this.switchCursor.switchCursor(1);
+			}
+			if(this.keyLeft.isDown) {
+				this.train.speed -= Train.SPEED_CHANGE_STEP;
+			}
+			if(this.keyRight.isDown) {
+				this.train.speed += Train.SPEED_CHANGE_STEP;
+			}
+			if(this.keySpace.isDown) {
+				this.restart();
+			}
 		}
 
 		this.cartsText.text = this.train.getCartCount();
+
+		if(this.train.isCrashed) {
+			this.crashedText.visible = true;
+		}
+	}
+
+	restart() {
+		this.unload();
+		this.load();
 	}
 
 	unload() {
 		this.removeChild(this.world);
 		this.removeChild(this.train);
 		this.removeChild(this.switchCursor);
+		this.removeChild(this.cartsText);
+		this.removeChild(this.crashedText);
 		
 		this.keyUp.close();
 		this.keyDown.close();
 		this.keyLeft.close();
 		this.keyRight.close();
+		this.keySpace.close();
 
 		this.keyUp = undefined;
 		this.keyDown = undefined;
 		this.keyLeft = undefined;
 		this.keyRight = undefined;
+		this.keySpace = undefined;
 
 		this.world = undefined;
 		this.train = undefined;
 		this.switchCursor = undefined;
+		this.cartsText = undefined;
+		this.crashedText = undefined;
 	}
 }
--- src/world/Train.js
+++ src/world/Train.js
@@ -8,62 +8,97 @@ export default class Train extends PIXI.Container {
 		this.world = world;
 		this.direction = direction;
 		this.speed = Train.INITIAL_SPEED;
+		this.isCrashed = false;
 
 		this.addCart();
 
 		this.locomotive = this.children[0];
-		this.locomotive.x = 0;
-		this.locomotive.y = 0;
+		this.locomotive.x = x * World.PIECE_WIDTH;
+		this.locomotive.y = y * World.PIECE_HEIGHT;
 		
 		this.path = this.world.getPath(0, 0, this.direction);
 	}
 
 	move() {
-		var getY = (x) => {
-			var y1 = World.PIECE_HEIGHT * this.path[Math.floor(x / World.PIECE_WIDTH)];
-			var y2 = World.PIECE_HEIGHT * this.path[Math.floor(x / World.PIECE_WIDTH) + 1];
-			var t = (x % World.PIECE_WIDTH) / World.PIECE_WIDTH;
-			return y1 + t * (y2 - y1);
-		}
-		var getAngle = (x) => {
-			var y1 = getY(x - Train.CART_WIDTH2);
-			var y2 = getY(x + Train.CART_WIDTH2);
-			return Math.tan((y2 - y1) / Train.CART_WIDTH);
-		}
+		var pieceX = Math.floor(this.locomotive.x / World.PIECE_WIDTH);
 
-		//move locomotive
-		this.locomotive.x += this.direction * this.speed;
-		this.locomotive.y = getY(this.locomotive.x);
-		this.locomotive.rotation = getAngle(this.locomotive.x);
-		
-		//check for cart on rail
-		var x = Math.floor(this.locomotive.x / World.PIECE_WIDTH);
-		var y = this.path[x];
-		if(y !== undefined && this.world.rails[x][y].isCart) {
-			this.addCart();
-			this.world.rails[x][y].isCart = false;
-			this.world.updateRailPiece(x, y);
-		}
+		if(!this.isCrashed && pieceX < this.path.length - 1) {
+			var pieceY = this.path[pieceX];
 
-		//move other carts
-		for(var i = 1; i < this.children.length; i++) {
-			var dx = this.children[i].x - this.children[i-1].x;
-			var dy = this.children[i].y - this.children[i-1].y;
-			var l = Train.CART_DELAY / Math.sqrt(dx * dx + dy * dy);
-			dx = dx * l;
-			dy = dy * l;
-			this.children[i].x = this.children[i-1].x + dx;
-			this.children[i].y = this.children[i-1].y + dy;
-			this.children[i].rotation = getAngle(this.children[i].x);
-
-			var skew = this.children[i].y - getY(this.children[i].x);
-
-			if(Math.abs(skew) > Train.CART_MAX_SKEW) {
-				this.children[i].y = getY(this.children[i].x) + Math.sign(skew) * Train.CART_MAX_SKEW;
-				skew = Math.sign(skew) * Train.CART_MAX_SKEW;
+			var getY = (x) => {
+				if(x < 0) {
+					return 0;
+				}
+				var y1 = World.PIECE_HEIGHT * this.path[Math.floor(x / World.PIECE_WIDTH)];
+				var y2 = World.PIECE_HEIGHT * this.path[Math.floor(x / World.PIECE_WIDTH) + 1];
+				if(Object.is(y2, NaN)) {
+					console.log(y1);
+					y2 = y1;
+				}
+				var t = (x % World.PIECE_WIDTH) / World.PIECE_WIDTH;
+				return y1 + t * (y2 - y1);
+			}
+			var getAngle = (x) => {
+				if(x < 0) {
+					return 0;
+				}
+				var y1 = getY(x - Train.CART_WIDTH2);
+				var y2 = getY(x + Train.CART_WIDTH2);
+				return Math.atan((y2 - y1) / Train.CART_WIDTH);
 			}
 
-			this.children[i].children[0].y = -Train.CART_HEIGHT2 - skew;
+			//move locomotive
+			this.locomotive.x += this.direction * this.speed;
+			this.locomotive.y = getY(this.locomotive.x);
+			this.locomotive.rotation = getAngle(this.locomotive.x);
+			
+			//check for cart on rail
+			if(this.world.rails[pieceX][pieceY].isCart) {
+				this.addCart();
+				this.world.rails[pieceX][pieceY].isCart = false;
+				this.world.updateRailPiece(pieceX, pieceY);
+			}
+
+			//move other carts
+			for(let i = 1; i < this.children.length; i++) {
+				let dx = this.children[i].x - this.children[i-1].x;
+				let dy = this.children[i].y - this.children[i-1].y;
+				let l= Train.CART_DELAY / Math.sqrt(dx * dx + dy * dy);
+				dx *= l;
+				dy *= l;
+				this.children[i].x = this.children[i-1].x + dx;
+				this.children[i].y = this.children[i-1].y + dy;
+				this.children[i].rotation = getAngle(this.children[i].x);
+
+				var skew = this.children[i].y - getY(this.children[i].x);
+
+				if(Math.abs(skew) > Train.CART_MAX_SKEW) {
+					this.children[i].y = getY(this.children[i].x) + Math.sign(skew) * Train.CART_MAX_SKEW;
+					skew = Math.sign(skew) * Train.CART_MAX_SKEW;
+				}
+
+				this.children[i].children[0].y = -Train.CART_HEIGHT2 - skew;
+			}
+		} else {
+			this.isCrashed = true;
+
+			//move locomotive
+			this.speed *= 0.96;
+			this.locomotive.rotation += Math.min(3, this.speed) * (Math.random() - 0.5) * 0.2;
+			this.locomotive.x += this.speed * Math.cos(this.locomotive.rotation);
+			this.locomotive.y += this.speed * Math.sin(this.locomotive.rotation);
+
+			//move other carts
+			for(let i = 1; i < this.children.length; i++) {
+				let dx = this.children[i].x - this.children[i-1].x;
+				let dy = this.children[i].y - this.children[i-1].y;
+				let l = Train.CART_DELAY / Math.sqrt(dx * dx + dy * dy);
+				dx *= l;
+				dy *= l;
+				this.children[i].x = this.children[i-1].x + dx;
+				this.children[i].y = this.children[i-1].y + dy;
+				this.children[i].rotation = Math.min(Math.max(Math.atan(dy / dx), -1), 1);
+			}
 		}
 	}
 
@@ -124,4 +159,4 @@ Train.CART_HEIGHT2 = Train.CART_HEIGHT / 2;
 Train.CART_DELAY = 32;
 Train.CART_MAX_SKEW = Train.CART_HEIGHT / 3;
 Train.INITIAL_SPEED = 0.4;
-Train.SPEED_CHANGE_STEP = 0.05;
+Train.SPEED_CHANGE_STEP = 0.03;
