bb87bb299dfc01beee56b2029fdbc35cf1b6fd3a
Linzee <d.gmiterko@gmail.com>
2017-05-04 12:45:55 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","2cef476acc70efc7ec2f2cbc953d12dc"],["manifest.json","73152c431bbfad2389b18a60fbc67b98"],["static/js/main.334f5a39.js","c6d91fb7b5d5411ad66c014d6e553732"]];
+var precacheConfig = [["index.html","e0813b05db3bd98004ee7f147a28c9a8"],["manifest.json","73152c431bbfad2389b18a60fbc67b98"],["static/js/main.a70d4241.js","84fb3ed7f788e295e41f3c690275e4ca"]];
 var cacheName = 'sw-precache-v2-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
