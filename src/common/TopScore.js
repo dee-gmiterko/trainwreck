2bbd77c4ab9b76cd377af9704994c448a50e3eba
Linzee <d.gmiterko@gmail.com>
2017-05-04 11:57:35 +0200

    AnalyticsTrain

--- src/common/TopScore.js
+++ src/common/TopScore.js
@@ -11,6 +11,12 @@ export default class TopScore {
 		this.topScore.push(score);
 		this.topScore.sort((a, b) => {return parseInt(b, 10) - parseInt(a, 10)});
 		window.localStorage.topScore = JSON.stringify(this.topScore);
+
+		if(window.ga) {
+			ga('send', 'event', 'Game', 'score', {
+				"metric5": score
+			});
+		}
 	}
 
 	getTop(count) {
