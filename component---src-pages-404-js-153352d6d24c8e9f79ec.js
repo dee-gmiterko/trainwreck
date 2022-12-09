202c16efff895e6f40d392e9426e99ec04a989d0
Dominik Gmiterko <d.gmiterko@gmail.com>
2022-12-09 15:35:44 +0100

    Updates

---
+++ component---src-pages-404-js-153352d6d24c8e9f79ec.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktrainwreck=self.webpackChunktrainwreck||[]).push([[883],{429:function(e,t,a){a.r(t);var n=a(7294),r=a(3060);t.default=function(e){var t=e.data;return n.createElement(r.Z,{title:"Not Found",siteMetadata:t.site.siteMetadata},n.createElement("h1",null,"Page not found"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-153352d6d24c8e9f79ec.js.map
